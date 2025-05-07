cb061faab46a67aa0aeee88d7ef92f01158b4e9a
Daehyun94 <sin_sit@naver.com>
2025-05-07 17:44:58 +0900

    DAO commit

--- src/com/shinhan/GitProject/GitProjectDAO.java
+++ src/com/shinhan/GitProject/GitProjectDAO.java
@@ -1,5 +1,29 @@
 package com.shinhan.GitProject;
 
+import java.util.List;
+
+
 public class GitProjectDAO {
 
+	//희정
+	private List<GitProjectDTO> selectAll() {
+		return null;
+	}
+	
+	//대현
+	private int Insert() {
+		return 0;
+		
+	}
+	
+	//진호
+	private int update() {
+		return 0;
+		
+	}
+	
+	//원정
+	private int delete() {
+		return 0;
+	}
 }
